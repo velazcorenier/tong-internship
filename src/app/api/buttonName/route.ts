5d602d2b10ccb8c4bf43b5c781a19b6a8f7b9fe4
Renier Velazco <renier.velazco94@gmail.com>
2023-06-07 20:50:02 -0400

    Create change color butt that calls the API

--- src/app/api/buttonName/route.ts
+++ src/app/api/buttonName/route.ts
@@ -10,11 +10,27 @@ const data: Data[] = [
     { id: '1', name: 'Button 1', color: 'red' },
     { id: '2', name: 'Button 2', color: 'blue' },
     { id: '3', name: 'Button 3', color: 'green' },
+    { id: '4', name: 'Button 4', color: 'yellow' },
+    { id: '5', name: 'Button 5', color: 'purple' },
+    { id: '6', name: 'Button 6', color: 'orange' },
+    { id: '7', name: 'Button 7', color: 'pink' },
+    { id: '8', name: 'Button 8', color: 'brown' },
+    { id: '9', name: 'Button 9', color: 'black' },
+    { id: '10', name: 'Button 10', color: 'white' },
+    { id: '11', name: 'Button 11', color: 'gray' },
+    { id: '12', name: 'Button 12', color: 'cyan' },
+    { id: '13', name: 'Button 13', color: 'magenta' },
+    { id: '14', name: 'Button 14', color: 'lime' },
+    { id: '15', name: 'Button 15', color: 'teal' },
+    { id: '16', name: 'Button 16', color: 'lavender' },
+    { id: '17', name: 'Button 17', color: 'maroon' },
+    { id: '18', name: 'Button 18', color: 'navy' },
+    { id: '19', name: 'Button 19', color: 'olive' },
+    { id: '20', name: 'Button 20', color: 'silver' },
 ];
 
 export async function GET(request: Request) {
         return new Response(JSON.stringify(
             data[Math.floor(Math.random() * data.length)]), {
-                status: 200,
             })
 }
--- src/components/IncrementButton/index.tsx
+++ src/components/IncrementButton/index.tsx
@@ -2,37 +2,33 @@
 import React, { useState, useEffect} from 'react';
 
 const IncrementButton = () => {
-    const [count, setCount] = useState(0);
     const [buttonName, setButtonName] = useState();
+    const [buttonColor, setColor] = useState();
+    
 
-    const handleIncrement = () => {
-        setCount(count + 1);
-    };
-
-    const handleDecrement = () => {
-        setCount(count - 1);
-    };
-
-    useEffect(() => {
+    const handleColorChange = () => {
         (async () => {
             const res = await fetch('/api/buttonName');
             console.log(res);
             const data = await res.json();
             console.log(data);
             setButtonName(data.name);
+            setColor(data.color);
         })();
+    };
+
+    useEffect(() => {
+        handleColorChange();
     }, []);
 
     return (
         <div className="flex items-center justify-center space-x-4 bg-white p-4 rounded shadow">
-            <p>{buttonName}</p>
+            <div className="flex flex-col items-center">
+                <div style={{ backgroundColor: buttonColor, width: '20px', height: '20px' }}></div>
+            </div>
             {}
-            <button className="bg-gray-200 text-black p-2 rounded hover:bg-gray-300 transition-colors duration-200" onClick={handleDecrement}>
-                -
-            </button>
-            <span className="font-bold text-xl">{count}</span>
-            <button className="bg-gray-200 text-black p-2 rounded hover:bg-gray-300 transition-colors duration-200" onClick={handleIncrement}>
-                +
+            <button className="bg-gray-200 text-black p-2 rounded hover:bg-gray-300 transition-colors duration-200" onClick={handleColorChange}>
+                Change Color
             </button>
         </div>
     );
